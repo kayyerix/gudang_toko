ad48befa59aa070f3f1199d7f42f8139bddc3ae8
kayyerix <frederickraynard@gmail.com>
2025-10-04 09:47:09 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("app").innerHTML = `
-  <button onclick="alert('Tambah barang')">+ Tambah</button>
-  <button onclick="alert('Kurangi barang')">- Kurangi</button>
-  <button onclick="confirm('Yakin hapus?')">Delete Item</button>
-`;
